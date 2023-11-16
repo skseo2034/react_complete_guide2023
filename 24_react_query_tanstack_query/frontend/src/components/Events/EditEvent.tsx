03cb412d944618ddb7e57b0b4e209639571f6cf8
서수교 <sk.seo@navercorp.com>
2023-11-16 10:50:05 +0900

    2023-11-16 학습

--- 24_react_query_tanstack_query/frontend/src/components/Events/EditEvent.tsx
+++ 24_react_query_tanstack_query/frontend/src/components/Events/EditEvent.tsx
@@ -1,10 +1,20 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal';
 import EventForm from './EventForm';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
 
 export default function EditEvent() {
 	const navigate = useNavigate();
+	const params = useParams();
+
+	const { data, isPending, isError, error } = useQuery({
+		queryKey: ['evnetns', params.id],
+		queryFn: () => fetchEvent({ id: params.id }),
+	});
 
 	function handleSubmit(formData: any) {
 		// dummy
@@ -14,9 +24,35 @@ export default function EditEvent() {
 		navigate('../');
 	}
 
-	return (
-		<Modal onClose={handleClose}>
-			<EventForm inputData={null} onSubmit={handleSubmit}>
+	let content;
+
+	if (isPending) {
+		content = (
+			<div className="center">
+				<LoadingIndicator />
+			</div>
+		);
+	}
+
+	if (isError) {
+		content = (
+			<>
+				<ErrorBlock
+					title="Failed to load event"
+					message={error.message || 'Failed to load event. Please check your inputs and try again later.'}
+				/>
+				<div className="form-actions">
+					<Link to="../" className="button">
+						Okay
+					</Link>
+				</div>
+			</>
+		);
+	}
+
+	if (data) {
+		content = (
+			<EventForm inputData={data} onSubmit={handleSubmit}>
 				<Link to="../" className="button-text">
 					Cancel
 				</Link>
@@ -24,6 +60,8 @@ export default function EditEvent() {
 					Update
 				</button>
 			</EventForm>
-		</Modal>
-	);
+		);
+	}
+
+	return <Modal onClose={handleClose}>{content}</Modal>;
 }
