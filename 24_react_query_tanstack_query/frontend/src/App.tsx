e7290209b8ba5d3130e6a5b24e2f09087d879963
서수교 <sk.seo@navercorp.com>
2023-11-22 11:41:27 +0900

    2023-11-22 학습

--- 24_react_query_tanstack_query/frontend/src/App.tsx
+++ 24_react_query_tanstack_query/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Events from './components/Events/Events';
 import EventDetails from './components/Events/EventDetails';
 import NewEvent from './components/Events/NewEvent';
-import EditEvent from './components/Events/EditEvent';
+import EditEvent, { loader as editEventLoader, action as editEventAction } from './components/Events/EditEvent';
 import { queryClient } from './util/http';
 import { QueryClientProvider } from '@tanstack/react-query';
 
@@ -30,6 +30,8 @@ const router = createBrowserRouter([
 			{
 				path: '/events/:id/edit',
 				element: <EditEvent />,
+				loader: editEventLoader,
+				action: editEventAction,
 			},
 		],
 	},
--- 24_react_query_tanstack_query/frontend/src/components/Events/EditEvent.tsx
+++ 24_react_query_tanstack_query/frontend/src/components/Events/EditEvent.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, redirect, useNavigate, useParams, useSubmit, useNavigation } from 'react-router-dom';
 
 import Modal from '../UI/Modal';
 import EventForm from './EventForm';
@@ -9,37 +9,41 @@ import ErrorBlock from '../UI/ErrorBlock';
 
 export default function EditEvent() {
 	const navigate = useNavigate();
+	const { state } = useNavigation();
+	const submit = useSubmit();
 	const params = useParams();
 
-	const { data, isPending, isError, error } = useQuery({
+	const { data, /*isPending,*/ isError, error } = useQuery({
 		queryKey: ['evnetns', params.id],
 		queryFn: () => fetchEvent({ id: params.id }),
+		staleTime: 10000,
 	});
 
-	const { mutate } = useMutation({
-		mutationFn: updateEvent,
-		onMutate: async data => {
-			// 낙관적인 업데이트
-			const newEvent = data.event;
-			await queryClient.cancelQueries({ queryKey: ['events', params.id] }); // 낙관적인 업데이트를 함으로 패치하지 않도록 한다.
-			const previousEvent = queryClient.getQueryData(['events', params.id]);
-
-			queryClient.setQueryData(['events', params.id], newEvent);
-
-			return { previousEvent };
-		},
-		onError: (error, data, context) => {
-			queryClient.setQueryData(['events', params.id], context?.previousEvent);
-		},
-		onSettled: async () => {
-			// 성공하던 실패 하던 무조건 호출 된다.
-			await queryClient.invalidateQueries({ queryKey: ['events', params.id] });
-		},
-	});
+	// const { mutate } = useMutation({
+	// 	mutationFn: updateEvent,
+	// 	onMutate: async data => {
+	// 		// 낙관적인 업데이트
+	// 		const newEvent = data.event;
+	// 		await queryClient.cancelQueries({ queryKey: ['events', params.id] }); // 낙관적인 업데이트를 함으로 패치하지 않도록 한다.
+	// 		const previousEvent = queryClient.getQueryData(['events', params.id]);
+	//
+	// 		queryClient.setQueryData(['events', params.id], newEvent);
+	//
+	// 		return { previousEvent };
+	// 	},
+	// 	onError: (error, data, context) => {
+	// 		queryClient.setQueryData(['events', params.id], context?.previousEvent);
+	// 	},
+	// 	onSettled: async () => {
+	// 		// 성공하던 실패 하던 무조건 호출 된다.
+	// 		await queryClient.invalidateQueries({ queryKey: ['events', params.id] });
+	// 	},
+	// });
 
 	function handleSubmit(formData: any) {
-		mutate({ id: params.id, event: formData });
-		navigate('../');
+		// mutate({ id: params.id, event: formData });
+		// navigate('../');
+		submit(formData, { method: 'PUT' });
 	}
 
 	function handleClose() {
@@ -48,13 +52,13 @@ export default function EditEvent() {
 
 	let content;
 
-	if (isPending) {
-		content = (
-			<div className="center">
-				<LoadingIndicator />
-			</div>
-		);
-	}
+	// if (isPending) {
+	// 	content = (
+	// 		<div className="center">
+	// 			<LoadingIndicator />
+	// 		</div>
+	// 	);
+	// }
 
 	if (isError) {
 		content = (
@@ -75,15 +79,37 @@ export default function EditEvent() {
 	if (data) {
 		content = (
 			<EventForm inputData={data} onSubmit={handleSubmit}>
-				<Link to="../" className="button-text">
-					Cancel
-				</Link>
-				<button type="submit" className="button">
-					Update
-				</button>
+				{state === 'submitting' ? (
+					<p>Sending data...</p>
+				) : (
+					<>
+						<Link to="../" className="button-text">
+							Cancel
+						</Link>
+						<button type="submit" className="button">
+							Update
+						</button>
+					</>
+				)}
 			</EventForm>
 		);
 	}
 
 	return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export async function loader({ params }: { params: any }) {
+	return await queryClient.fetchQuery({
+		queryKey: ['evnetns', params.id],
+		queryFn: () => fetchEvent({ id: params.id }),
+	});
+}
+
+export async function action({ request, params }: { request: any; params: any }) {
+	const formData = await request.formData();
+	const updatedEventData = Object.fromEntries(formData);
+	await updateEvent({ id: params.id, event: updatedEventData });
+	await queryClient.invalidateQueries({ queryKey: ['events'] });
+
+	return redirect('../');
 }
