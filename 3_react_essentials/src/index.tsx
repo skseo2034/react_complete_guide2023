bf7b1702d6150ac9f1679190648595ed3c9c37a2
서수교 <sk.seo@navercorp.com>
2023-11-29 12:05:26 +0900

    2023-11-29 학습

--- 3_react_essentials/src/index.tsx
+++ 3_react_essentials/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import App from './App.js';
+import App from './App';
 import './index.css';
 
 const entryPoint = document.getElementById('root') as HTMLElement;
